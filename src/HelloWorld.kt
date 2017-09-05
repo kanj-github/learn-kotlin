9f01735934cdb759adde5ddc973b95f8379cdf07
kanj <murderthemuggles@yahoo.co.in>
2017-09-05 17:12:22 +0530

    Save code

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,6 +1,16 @@
 fun main(args: Array<String>) {
-    val data = mapOf("emails" to "mail.domain.com")
+    val data = mapOf("email" to "mail@domain.com")
     val email = data["email"] ?: "No email"
     println("-------")
     println(email)
+
+    val text = """
+    |Tell me and I forget.
+    |Teach me and I remember.
+    |Involve me and I learn.
+    |
+    |(Benjamin Franklin)
+    """.trimMargin()
+
+    println(text)
 }

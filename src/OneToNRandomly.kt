eddc9d92d12cec6bf9931b505c7e3ba2dae43c5d
kanj <murderthemuggles@yahoo.co.in>
2017-09-28 11:39:35 +0530

    Print 1 to N randomly

---
+++ src/OneToNRandomly.kt
@@ -0,0 +1,33 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+val random = Random();
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("fuck you!")
+        return
+    }
+
+    try {
+        val arr:ArrayList<Int> = ArrayList()
+        for (i in 1..args[0].toInt()) {
+            arr.add(i)
+        }
+        printOneByOne(arr)
+    } catch (e: NumberFormatException) {
+        println("fuck you!")
+    }
+}
+
+tailrec fun printOneByOne(arr: ArrayList<Int>) {
+    val bound = arr.size - 1
+    if (bound > 0) {
+        val index = random.nextInt(bound)
+        println(arr[index])
+        arr.removeAt(index)
+        printOneByOne(arr)
+    } else {
+        println(arr[0])
+    }
+}

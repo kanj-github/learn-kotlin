7f4b846d5f600bed0e8db06d13cd97260d854055
kanj <murderthemuggles@yahoo.co.in>
2017-10-10 10:35:07 +0530

    Print N1 to N2 in random order

--- src/OneToNRandomly.kt
+++ src/OneToNRandomly.kt
@@ -2,19 +2,35 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 val random = Random();
+val usageText = """
+|Usage:
+|    main(N) - print 1 to N in random order
+|    main(N1, N2) - print N1 to N2 in random order
+""".trimMargin()
 
+// Prints N1 to N2 in random order.
 fun main(args: Array<String>) {
-    if (args.size != 1) {
-        println("fuck you!")
+    if (args.isEmpty() || args.size > 2) {
+        println(usageText)
         return
     }
 
     try {
+        val start: Int
+        val end: Int
+        if (args.size == 2) {
+            start = args[0].toInt()
+            end = args[1].toInt()
+        } else {
+            start = 1
+            end = args[0].toInt()
+        }
+
         val arr:ArrayList<Int> = ArrayList()
-        /*for (i in 1..args[0].toInt()) {
+        /*for (i in start..end) {
             arr.add(i)
         }*/
-        (1..args[0].toInt()).mapTo(arr) {it}
+        (start..end).mapTo(arr) {it}
         printOneByOne(arr)
     } catch (e: NumberFormatException) {
         println("fuck you!")
